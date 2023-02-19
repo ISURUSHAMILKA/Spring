f72be0b9396b3ff9d5d6ea901b8d6c9f1506d4eb
ISURUSHAMILKA <isurushamilka@gmail.com>
2023-02-19 19:15:40 +0530

    intial version

---
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -0,0 +1,13 @@
+package com.example.demo.dao;
+
+import java.util.UUID;
+
+public class FakePersonDataAccessService implements PersonDao{
+
+
+    private static List<Person> 
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+}
